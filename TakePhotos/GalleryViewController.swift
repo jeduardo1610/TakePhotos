8eab75d0a4827916ddab44587a88bc522a5abd17
Jorge Eduardo <jeduardo1601@gmail.com>
2018-01-25 16:12:10 -0600

    Adding design for Gallery view controller and its segue from main view controller

---
+++ TakePhotos/GalleryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GalleryViewController.swift
+//  TakePhotos
+//
+//  Created by Jorge Eduardo on 25/01/18.
+//  Copyright © 2018 Jorge Eduardo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GalleryViewcontroller : UIViewController{
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+    
+}
